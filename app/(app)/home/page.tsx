db670931a647992f556e6461e97fc9119d0ea7e8
abhi-github-159084 <abhishekabhi159084@gmail.com>
2025-02-12 14:29:00 +0530

    initial commit

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+import { useState,useEffect,useCallback } from 'react'
+import axios from 'axios'
+import VideoCard from '@/components/VideoCard'
 import React from 'react'
+
 
 const Home = () => {
   return (
