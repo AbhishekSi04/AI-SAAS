49c0f3b7a6e2521e9fcd4619fe7c1149a2bed11a
abhi-github-159084 <abhishekabhi159084@gmail.com>
2025-03-03 19:44:31 +0530

    changes page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -117,14 +117,12 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-900 text-white">
-      <h1 className="text-4xl sm:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 drop-shadow-lg text-center">
-        SAAS - Software as a Service
-      </h1>
+        <h1 className="text-5xl sm:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-pulse drop-shadow-xl transition-all duration-500 hover:scale-110">
+          MediaMorph
+        </h1>
 
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h2 className="text-5xl sm:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-pulse drop-shadow-xl transition-all duration-500 hover:scale-110">
-          MediaMorph
-        </h2>
+        
 
         {!isSignedIn ? (
           <div className="flex flex-col gap-4 items-center mx-auto mt-5" >
